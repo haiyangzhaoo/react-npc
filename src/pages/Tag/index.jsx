0416b14c4356b065df32c34350da6ed4a9bf243b
wb-zhy452461 <wb-zhy452461@alibaba-inc.com>
2020-11-25 16:06:59 +0800

    feat: tag页修改为三栏布局

--- src/pages/Tag/index.jsx
+++ src/pages/Tag/index.jsx
@@ -4,7 +4,13 @@ import './index.less'
 export default function Tag() {
   return (
     <div className="tag">
-      Tag
+      <header className="tag-header">Header</header>
+      <div className="tag-body">
+        <div>Left</div>
+        <div>Center</div>
+        <div>Right</div>
+      </div>
+      <footer className="tag-footer">Fotter</footer>
     </div>
   )
 }
